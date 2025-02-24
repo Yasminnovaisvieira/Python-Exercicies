fdf905fa83340bc1f35c90519365534ad70bf8d5
Yasminnovaisvieira <yasminnovaisvieira789@gmail.com>
2025-02-24 20:19:30 -0300

    Exemplos de Python

---
+++ Raiz_Cubica.py
@@ -0,0 +1,9 @@
+# Calcule a raiz cúbica de um número.
+
+print(f"Olá! O programa irá calcular a POTÊNCIA de um número inteiro elevado a um expoente.\n")
+
+numero = float(input("Digite um número: "))
+
+raiz_cubica = numero ** (1 / 3)
+
+print(f"A raiz cúbica de {numero} é aproximadamente: {raiz_cubica:.2f}")
